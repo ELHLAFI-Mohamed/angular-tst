42216919a8e4702bbf0c3cd88802575be7d86b56
elhlafi mohamed <mohamed.elhlafi.ensias@gmail.com>
2020-06-06 13:38:28 +0100

    version 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+     MatListModule,
     FlexLayoutModule
   ],
   providers: [],
